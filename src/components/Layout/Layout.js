ca652b5ca2d07196eecc7a9285bb3db69070944b
Will <mack.mindenhall@gmail.com>
2020-03-04 20:47:02 -0500

    Creation of Burger Builder app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
+const layout = ( props ) => (
+    <Auxiliary>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className="Content">
+            {props.children}
+        </main>
+    </Auxiliary>
+);
+
+export default layout;
